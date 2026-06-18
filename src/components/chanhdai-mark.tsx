4316662ae7d91ba7ce74c283b6366ceb3e93be4c
Mandeep Nehra <mandeepnehra.work@gmail.com>
2026-06-18 18:35:29 +0000

    fix: replace MN monogram with Deep logotype

--- src/components/chanhdai-mark.tsx
+++ src/components/chanhdai-mark.tsx
@@ -3,24 +3,25 @@ export function ChanhDaiMark(props: React.ComponentProps<"svg">) {
     <svg
       xmlns="http://www.w3.org/2000/svg"
       fill="none"
-      viewBox="0 0 520 256"
+      viewBox="0 0 320 96"
       aria-hidden
       {...props}
     >
-      {/* M */}
-      <path
+      <text
+        x="0"
+        y="80"
+        fontFamily="'Inter', 'Helvetica Neue', Arial, sans-serif"
+        fontWeight="800"
+        fontSize="88"
         fill="currentColor"
-        d="M0 0h64l56 120L176 0h64v256h-64V112l-40 88H96l-40-88v144H0V0Z"
-      />
-      {/* N */}
-      <path
-        fill="currentColor"
-        d="M280 0h64l96 152V0h64v256h-64L344 104v152h-64V0Z"
-      />
+        letterSpacing="-4"
+      >
+        Deep
+      </text>
     </svg>
   )
 }
 
 export function getMarkSVG() {
-  return `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 520 256"><path fill="currentColor" d="M0 0h64l56 120L176 0h64v256h-64V112l-40 88H96l-40-88v144H0V0Z"/><path fill="currentColor" d="M280 0h64l96 152V0h64v256h-64L344 104v152h-64V0Z"/></svg>`
+  return `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 320 96"><text x="0" y="80" font-family="'Inter', 'Helvetica Neue', Arial, sans-serif" font-weight="800" font-size="88" fill="currentColor" letter-spacing="-4">Deep</text></svg>`
 }
